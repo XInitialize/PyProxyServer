8808ab0c5826ccd9a060c836b3da99f10afa132c
root <twistedscau@vip.qq.com>
2022-08-15 14:07:31 +0800

    update

--- PyProxyServer/port_forward.py
+++ PyProxyServer/port_forward.py
@@ -1,11 +1,25 @@
 import socket
-import threading
 
-def socket_thread(host,port,conn,addr):
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((host,port))
-    sock.
 
+def socket_thread(host, port, conn: socket.socket, addr):
+    sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    pp = conn.recv(4096)
+    print(*addr, "REQ", pp)
+    sock.send(pp)
+    dd = sock.recv(4096)
+    conn.send(dd)
+    print(*addr, "RESP", dd)
+    while dd:
+        dd = sock.recv(4096)
+        conn.send(dd)
+        print(*addr, "RESP", dd)
+    print(*addr, "OVER")
+
+    sock.shutdown(1)
+    sock.close()
+    conn.shutdown(1)
+    conn.close()
 
 
 class PortForward:
@@ -15,13 +29,16 @@ class PortForward:
         self.conn_host = conn_host
         self.conn_port = conn_port
 
-        self.sock_listen = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
+        self.sock_listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock_listen.bind((self.listen_host, self.listen_port))
 
     def start(self):
         self.sock_listen.listen()
 
-
     def loop(self):
         while True:
-            conn,addr = self.sock_listen.accept()
+            conn, addr = self.sock_listen.accept()
+            # threading.Thread(target=socket_thread, args=[self.conn_host, self.conn_port, conn, addr]).start()
+            socket_thread(self.conn_host, self.conn_port, conn, addr)
+            print("**started**")
