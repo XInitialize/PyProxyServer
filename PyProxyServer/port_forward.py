76c058845fdb866126dd456dd8a13b047332ae24
root <twistedscau@vip.qq.com>
2022-08-15 15:32:34 +0800

    update

--- PyProxyServer/port_forward.py
+++ PyProxyServer/port_forward.py
@@ -1,19 +1,46 @@
 import socket
+import threading
 
 
 def socket_thread(host, port, conn: socket.socket, addr):
     sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
     sock.connect((host, port))
     pp = conn.recv(4096)
-    print(*addr, "REQ", pp)
+    print(*addr, "REQ ", pp)
+
     sock.send(pp)
-    dd = sock.recv(4096)
+    try:
+        dd = sock.recv(4096)
+    except TimeoutError:
+        sock.shutdown(1)
+        sock.close()
+        conn.shutdown(1)
+        conn.close()
+        return
+    except ConnectionResetError as e:
+        print(*addr, "RESET", e)
+        sock.shutdown()
+        sock.close()
+        conn.shutdown(1)
+        conn.close()
+        return
     conn.send(dd)
     print(*addr, "RESP", dd)
     while dd:
-        dd = sock.recv(4096)
-        conn.send(dd)
-        print(*addr, "RESP", dd)
+        try:
+            dd = sock.recv(4096)
+            conn.send(dd)
+            print(*addr, "RESP", dd)
+        except TimeoutError:
+            break
+        except ConnectionResetError as e:
+            print(*addr, "RESET", e)
+            sock.shutdown(1)
+            sock.close()
+            conn.shutdown(1)
+            conn.close()
+            return
     print(*addr, "OVER")
 
     sock.shutdown(1)
@@ -33,12 +60,32 @@ class PortForward:
         self.sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock_listen.bind((self.listen_host, self.listen_port))
 
+    @classmethod
+    def from_configs(cls, config_file: str):
+        import configparser
+        cp = configparser.ConfigParser()
+        cp.read(config_file)
+        return cls(listen_host=cp.get("listen", "host"), listen_port=cp.get("listen", "port"),
+                   conn_host=cp.get("connection", "host"), conn_port=cp.get("connection", "port"))
+
     def start(self):
         self.sock_listen.listen()
 
+    def close(self):
+        print("SOCKET HANDLE", *self.sock_listen.getsockname(), "CLOSED.")
+        self.sock_listen.shutdown(1)
+        self.sock_listen.close()
+
     def loop(self):
         while True:
-            conn, addr = self.sock_listen.accept()
-            # threading.Thread(target=socket_thread, args=[self.conn_host, self.conn_port, conn, addr]).start()
-            socket_thread(self.conn_host, self.conn_port, conn, addr)
-            print("**started**")
+            try:
+                conn, addr = self.sock_listen.accept()
+            except KeyboardInterrupt:
+                break
+
+            # threading
+            threading.Thread(target=socket_thread, args=[self.conn_host, self.conn_port, conn, addr]).start()
+            # blocking
+            # socket_thread(self.conn_host, self.conn_port, conn, addr)
+            print(*addr, "**RECV**")
+        self.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyProxyServer.port_forward import PortForward
 
 if __name__ == '__main__':
-    pf = PortForward("0.0.0.0", 16666, "127.0.0.1", 16667)
+    pf = PortForward("0.0.0.0", 16666, "localhost", 6379)
     pf.start()
     pf.loop()
